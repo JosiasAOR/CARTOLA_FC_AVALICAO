74c3e12281aa5f9c24abeb084bc71b52d73b4ae7
josiasaor <josiasaor@gmail.com>
2023-10-05 13:59:46 -0300

    new atualizatio

--- src/Pages/jogadores.jsx
+++ src/Pages/jogadores.jsx
@@ -1,53 +1,66 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 
 export default function Jogadores() {
-    const [data, setData] = useState([]);
-    const [show, setShow] = useState(false);
-    const { nome } = useParams()
+  const [data, setData] = useState([]);
+  const [show, setShow] = useState(false);
+  const { nome } = useParams();
 
-    useEffect(() => {
-        apiCartolaFC();
-    }, []);
+  useEffect(() => {
+    apiCartolaFC();
+  }, []);
 
-    function apiCartolaFC() {
-        axios.get(`https://api.cartola.globo.com/atletas/mercado/${nome}`)
-            .then((response) => {
-                const joadores = response.data;
-                setData(joadores.atletas);
-                console.log(joadores.atletas)
-                setShow(true);
-            })
-            .catch((error) => {
-                console.error("Erro ao buscar os dados:", error);
-            });
+  function apiCartolaFC() {
+    axios
+      .get(`https://api.cartola.globo.com/atletas/mercado/${nome}`)
+      .then((response) => {
+        const jogadores = response.data;
+        setData(jogadores.atletas || []);
+        setShow(true);
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar os dados:", error);
+        setData([]);
+        setShow(true);
+      });
+  }
+
+  function formatarURL(url) {
+    if (url) {
+      let foto = url.replace(/FORMATO/g, "220x220");
+      return foto;
+    } else {
+      return null;
     }
+  }
 
-
-    return (
-        <div>
-            {show && (
-                <ul>
-                    {data.map((jogador) => (
-                <div className="video-card">
-                  <p>{jogador.nome}</p>
-                  <img
-                    src={()=>{
-                        let url = jogador.foto;
-                        url = url.replace(/FORMATO/g, '220x220');
-                         return url
-                        }}
-                  />
-                <button onClick={()=>{
-                    let url = jogador.foto;
-                    url = url.replace(/FORMATO/g, '220x220');
-                    window.open(url);
-                    }}></button>
-                </div>
+  return (
+    <div>
+        <button onClick={()=>{
+            console.log(data)
+        }}>mostart</button>
+      {show ? (
+        data && data.length > 0 ? (
+          <ul>
+            <h1>{data.id_clube}</h1>
+            {data.map((jogador) => (
+              <div className="video-card" key={jogador.id}>
+                <p>{jogador.nome}</p>
+                {jogador.foto ? (
+                  <img src={formatarURL(jogador.foto)} alt={jogador.nome} />
+                ) : (
+                  <p>No photo available</p>
+                )}
+              </div>
             ))}
-                </ul>
-            )}
-        </div>
-    )
+          </ul>
+        ) : (
+          <p>Sem foto de Jogadores</p>
+        )
+      ) : (
+        <p>carregando...</p>
+      )}
+    </div>
+  );
 }
